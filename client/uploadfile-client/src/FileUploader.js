484e74d34b770faada4d6c1b137013b2dc22bae5
0XCube <arnold.zhang.jlu@gmail.com>
2024-10-15 01:32:42 +0800

    静态搭建

---
+++ client/uploadfile-client/src/FileUploader.js
@@ -0,0 +1,12 @@
+import { InboxOutlined } from "@ant-design/icons";
+import "./FileUploader.css";
+
+function FileUploader() {
+  return (
+    <div className="upload-container">
+      <InboxOutlined />
+    </div>
+  );
+}
+
+export default FileUploader;

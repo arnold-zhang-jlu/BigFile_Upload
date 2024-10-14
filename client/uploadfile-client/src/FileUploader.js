c8b4dfc61b6b152fa76e13d031c72ea6e4639b61
0XCube <arnold.zhang.jlu@gmail.com>
2024-10-15 02:59:01 +0800

    实现文件拖曳上传至本地，并实现预览

--- client/uploadfile-client/src/FileUploader.js
+++ client/uploadfile-client/src/FileUploader.js
@@ -5,12 +5,42 @@ import useDrag from "./hooks/useDrag";
 
 function FileUploader() {
   const uploadContainerRef = useRef(null);
-  useDrag(uploadContainerRef);
+  const { selectedFile, filePreview } = useDrag(uploadContainerRef);
   return (
     <div className="upload-container" ref={uploadContainerRef}>
-      <InboxOutlined />
+      {/* <InboxOutlined /> */}
+      {renderFilePreview(filePreview)}
     </div>
   );
+}
+
+function renderFilePreview(filePreview) {
+  const { url, type } = filePreview;
+  if (url) {
+    if (type.startsWith("video/")) {
+      return (
+        <video
+          src={url}
+          alt="preview"
+          controls
+          style={{ maxWidth: "200px", maxHeight: "200px" }}
+        />
+      );
+    } else if (type.startsWith("image/")) {
+      return (
+        <img
+          src={url}
+          alt="preview"
+          controls
+          style={{ maxWidth: "200px", maxHeight: "200px" }}
+        />
+      );
+    } else {
+      return url;
+    }
+  } else {
+    return <InboxOutlined />;
+  }
 }
 
 export default FileUploader;
--- client/uploadfile-client/src/hooks/useDrag.js
+++ client/uploadfile-client/src/hooks/useDrag.js
@@ -1,17 +1,51 @@
+import { message } from "antd";
 import { useState, useEffect, useCallback } from "react";
+import { MAX_FILE_SIZE } from "../constant";
 
 function useDrag(uploadContainerRef) {
+  const [selectedFile, setSelectedFile] = useState(null);
+  //存放文件预览信息，url是预览地址，type是文件类型
+  const [filePreview, setFilePreview] = useState({ url: null, type: null });
+
   const handleDrag = useCallback((e) => {
     e.preventDefault();
     e.stopPropagation();
   }, []);
 
+  const checkFile = (files) => {
+    const file = files[0];
+    if (!file) {
+      message.error("没有选择任何文件");
+      return;
+    }
+
+    if (file.size > MAX_FILE_SIZE) {
+      message.error("文件大小不能超过2G");
+      return;
+    }
+
+    if (!(file.type.startsWith("image/") || file.type.startsWith("video/"))) {
+      message.error("文件类型必须是图片或视频");
+      return;
+    }
+    setSelectedFile(file);
+  };
+
   const handleDrop = useCallback((e) => {
     e.preventDefault();
     e.stopPropagation();
-    const { files } = e.dataTransfer;
-    console.log(files);
+    checkFile(e.dataTransfer.files);
   }, []);
+
+  useEffect(() => {
+    if (!selectedFile) return;
+    const url = URL.createObjectURL(selectedFile);
+    setFilePreview({ url, type: selectedFile.type });
+    return () => {
+      //revokeObjectURL可以撤销此url占用的资源
+      URL.revokeObjectURL(url);
+    };
+  }, [selectedFile]);
 
   useEffect(() => {
     const uploadContainer = uploadContainerRef.current;
@@ -27,7 +61,7 @@ function useDrag(uploadContainerRef) {
       uploadContainer.removeEventListener("dragleave", handleDrag);
     };
   }, []);
-  return {};
+  return { selectedFile, filePreview };
 }
 
 export default useDrag;
